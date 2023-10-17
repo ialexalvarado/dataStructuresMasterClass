6898af3ee6ff28a1e5fc4fbcf746fc96cac35132
ialexalvarado <ialejandroalvarado@gmail.com>
2023-10-16 20:33:15 -0600

    first part of course

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // run `node index.js` in the terminal
 
+import  maxSubarraySum  from "./files/slidingWindowPattern.js";
+
+console.log(maxSubarraySum([2,6,9,2,1,8,5,6,3],3))
+
 console.log(`Hello Node.js v${process.versions.node}!`);
